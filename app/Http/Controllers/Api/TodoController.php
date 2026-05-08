20de4001c2f3631b8470d6c0d7ec90e6f3a5807b
snsasaki <sn.sasaki@core-tech.jp>
2026-05-08 11:44:17 +0900

    :wrench: TodoControllerのディレクトリ変更

--- app/Http/Controllers/Api/TodoController.php
+++ app/Http/Controllers/Api/TodoController.php
@@ -1,12 +1,12 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Api;
 
+use App\Http\Controllers\Controller;
 use App\Models\User;
 use App\Services\TodoService;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
 
 class TodoController extends Controller
 {
--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\AuthController;
-use App\Http\Controllers\TodoController;
+use App\Http\Controllers\Api\TodoController;
 use App\Http\Middleware\TokenAuth;
 use Illuminate\Support\Facades\Route;
 
